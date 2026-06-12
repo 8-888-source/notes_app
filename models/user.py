7510c0c5b64724ce4cbbe11a73602f0994cbe810
8-888-source <kristinamaks777@mail.ru>
2026-06-12 06:15:14 +0500

    Set table name for User model to 'users'

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@ from extensions import db
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
-
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
 
     username = db.Column(db.String(50), unique=True, nullable=False)
